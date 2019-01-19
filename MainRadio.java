0e0522ef886c5fd3a1c174ae645758db1450d88e
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2019-01-18 23:08:11 -0600

    JUnit y UML

--- MainRadio.java
+++ MainRadio.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MainRadio {
     public static void main(String[] args){
+		
         String menu;
         int opcion, boton, cambiar;
         Scanner scan = new Scanner(System.in);
