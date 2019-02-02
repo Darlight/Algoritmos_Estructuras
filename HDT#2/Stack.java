3e6d3f4b459b17701d092c6ffb8ae8ef86e0a0f6
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2019-02-01 22:00:01 -0600

    Codigo documentado

--- HDT#2/Stack.java
+++ HDT#2/Stack.java
@@ -1,3 +1,12 @@
+/*
+Autores:    Mario Perdomo 18029
+            Josue Sagastume 18173
+
+Fecha: 01 de febrero de 2019
+Proposito: Esta clase es la interfaz de Stack, y es generica
+porque no se sabe con que tipo de dato se trabajara. La clase
+StackArrayList hereda sus metodos de esta interfaz.
+ */
 public interface Stack<E> {
 
     public void push(E item);
