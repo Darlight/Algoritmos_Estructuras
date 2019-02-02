05583293cd8f6b7557db3544960051d5820621cc
Darlight <andres.perdomo09@gmail.com>
2019-02-01 20:20:03 -0600

    UML ya preparados
    PORFIN!

--- HDT#2/MainCalculadora.java
+++ HDT#2/MainCalculadora.java
@@ -16,26 +16,35 @@ public class MainCalculadora {
 
         System.out.println("Ingrese la direccion del archivo: ");
         //C:\Users\josue\Desktop\prueba\prueba.txt
+        //Si aqui lee el archivo de texto
         nombreArchivo = scan.nextLine();
-
+        // Fijo tendra mas de 1 linea entonces
+        // Hay que hacer un arraylist de String aqui y por cada linea del texto
+        // se gaurdada en cada celda de la lista
+        //Aqui iria el for creo
         texto = archivo.leerTxt(nombreArchivo);
+        //Como Aqui
         String[] lista = texto.split(" ");
-
-        for (int i = 0; i < lista.length; i++){
-            if (lista[i].equals("+")){
-                pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "+"));
-            } else if (lista[i].equals("-")){
-                pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "-"));
-            } else if (lista[i].equals("*")){
-                pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "*"));
-            }else if (lista[i].equals("/")){
-                pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "/"));
-            } else {
-                pila.push(Integer.parseInt(lista[i]));
+        //F
+        for (int j = 0; j < lista[j]; j++){
+            for (int i = 0; i < lista[j].length; i++){
+                if (lista[i].equals("+")){
+                    pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "+"));
+                } else if (lista[i].equals("-")){
+                    pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "-"));
+                } else if (lista[i].equals("*")){
+                    pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "*"));
+                }else if (lista[i].equals("/")){
+                    pila.push(calculadora.Calculate(pila.pop(), pila.pop(), "/"));
+                } else {
+                    pila.push(Integer.parseInt(lista[i]));
+                }
+                System.out.println("El resultado es: " + pila.pop());
             }
         }
 
-        System.out.println("El resultado es: " + pila.pop());
+
+
 
     }
 }
