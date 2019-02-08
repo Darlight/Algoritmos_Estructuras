a2d6f366ac01be3f5a97409f10bbfb32ec5f851a
Darlight <andres.perdomo09@gmail.com>
2019-02-07 23:40:05 -0600

    COmienzo del lab 3

---
+++ HDT#3/Comparable.java
@@ -0,0 +1,10 @@
+/*
+Autores:    Mario Perdomo 18029
+            Josue Sagastume 18173
+
+Fecha: 07 de febrero de 2019
+Proposito:
+ */
+public interface Comparable{
+    public int[] Sorting (int[] numbers, String option);
+}
