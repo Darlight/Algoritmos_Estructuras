cca34f376270ccb905dc40487ce3a0b461e75711
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2019-02-08 23:30:12 -0600

    Solucion de errores

--- HDT#3/Bubblesort.java
+++ HDT#3/Bubblesort.java
@@ -6,18 +6,29 @@ Autores:    Mario Perdomo 18029
 Fecha: 07 de febrero de 2019
 Proposito:
  */
+import java.util.*;
+import java.io.*;
 public class BubbleSort implements Comparable{
 
     public int[] bubbleSort(Comparable[] numbers) {
-        for (int i = 0; i < (numbers.length)-1; i++)
-            for (int j = 0; j < (numbers.length-i)-1; j++)
-                if (arr[j] > arr[j+1])
+        for (int i = 0; i < numbers.length-1; i++)
+            for (int j = 0; j < numbers.length-i-1; j++)
+                if ((int)numbers[j] > (int)numbers[j+1])
                 {
                     // reemplaza arr[j+1] y arr[i]
-                    int temp = this.arr[j];
-                    this.arr[j] = this.arr[j+1];
-                    this.arr[j+1] = temp;
+                    int temp = (int)numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
                 }
-        return; arr[];
+        return numbers[];
+    }
+
+    public void sort(Comparable[] numbers){
+        if (numbers == null||numbers.length==0){
+            return;
+        }
+        this.array = numbers;
+        int length = numbers.length;
+        quickSort(0, (numbers.length)-1);
     }
 }
